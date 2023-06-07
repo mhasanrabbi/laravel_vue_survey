a2eafbeb6597d4ca94646c51386674c8e6697801
mhasanrabbi <mhmdhasanrabbi@gmail.com>
2023-06-07 19:17:35 +0600

    add login register component

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,29 +1,35 @@
-import  {createRouter, createWebHistory} from "vue-router";
-import Dashboard from "../views/Dashboard.vue"
-import Register from "../views/Register.vue"
-import Login from "../views/Login.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Dashboard',
-        component: Dashboard
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    }
+  {
+    path: "/",
+    redirect: "/dashboard",
+    component: DefaultLayout,
+    children: [
+        { path: "/dashboard", name: "Dashboard", component: Dashboard },
+        { path: "/surveys", name: "Surveys", component: Surveys },
+    ],
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
